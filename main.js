62478556d801497f2138d202df87326abe6721dd
DJouve <damien.jouve@hetic.net>
2022-03-01 22:30:30 +0100

    End of introduction

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ let userResults = [];
 const userFilters = {
   choice: "",
   chain: "",
-  date: "",
+  year: "",
   time: ""
 };
 
@@ -37,3 +37,36 @@ tvChainChoiceButton.addEventListener("click", () => {
   ChainChoice.classList.add("hide");
   userFilters.chain = "getChain?";
 });
+
+// Fonction to move the selector for the Years
+
+const tvYearSelector = document.querySelector(".slider")
+const tvYearDisplay = document.querySelector(".year_choice_display")
+
+tvYearDisplay.innerHTML = tvYearSelector.value;
+function updateTextInput(val) {
+  tvYearDisplay.innerHTML = val
+}
+
+//Third page Button
+
+const tvYearChoiceButton = document.querySelector(".button-year-choice");
+tvYearChoiceButton.addEventListener("click", () => {
+  const ChainChoice = document.querySelector(".tv_year_count");
+  ChainChoice.classList.add("hide");
+  userFilters.year = tvYearSelector.value;
+});
+
+//Fourth page Buttons
+
+const tvHourChoice = document.querySelectorAll(".hour_choice_selector");
+
+for (let i = 0; i< tvHourChoice.length; i++){
+  tvHourChoice[i].addEventListener("click", ()=> {
+    const hourChoice = document.querySelector(".tv_hour_count");
+    const hourValues = document.querySelectorAll(".hour_input")
+    hourChoice.classList.add("hide");
+    userFilters.time = hourValues[i].innerHTML
+    console.log(userFilters)
+  })
+}
